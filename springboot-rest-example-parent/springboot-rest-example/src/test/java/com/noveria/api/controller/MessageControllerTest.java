8805d15992e60ac4dbb7da24b45a39e0e10168fe
SleepingTalent <jaybono30@hotmail.com>
2017-10-06 08:41:26 +0100

    added initial project

---
+++ springboot-rest-example-parent/springboot-rest-example/src/test/java/com/noveria/api/controller/MessageControllerTest.java
@@ -0,0 +1,35 @@
+package com.noveria.api.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = MessageController.class)
+@EntityScan("com.noveria.api.model.domain")
+@ActiveProfiles("test")
+public class MessageControllerTest {
+
+    @Autowired
+    private MessageController messageController;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getMessages() {
+
+        String actual = messageController.getMessage();
+
+        assertEquals("Demo Message", actual);
+    }
+
+}
